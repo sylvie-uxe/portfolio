f542f69689eb429d8c3d6133de9231d63fcf4a00
sylvie-uxe <sylvie.uxe@gmail.com>
2021-07-01 12:36:53 -0400

    favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,7 @@ module.exports = {
         // Enables `Add to Homescreen` prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: `standalone`,
-        icon: `static/icon.png`, // This path is relative to the root of the site.
+        icon: `static/logo.png`, // This path is relative to the root of the site.
       },
     },
   ],
